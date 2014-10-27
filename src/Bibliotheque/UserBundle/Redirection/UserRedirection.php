9164bf4a2b181c42fb4940f6a213f7f37fd1175f
jarba3 <bjobard@gmail.com>
2014-10-27 11:45:37 +0100

    Redirection apres login

---
+++ src/Bibliotheque/UserBundle/Redirection/UserRedirection.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Bibliotheque\UserBundle\Redirection;
+
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class UserRedirection implements AuthenticationSuccessHandlerInterface
+{
+    private $router;
+
+    public function __construct(RouterInterface $router){
+        $this->router = $router;
+    }
+    
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token){
+        $roles = $token->getRoles();
+        
+        $rolesTab = array_map(function($role){
+                                        return $role->getRole();
+                                    }, $roles);
+        
+        
+        if (in_array("ROLE_BIBLIOTHECAIRE", $rolesTab)){
+
+            $redirection = new RedirectResponse($this->router->generate('bibliotheque_index'));
+        }
+
+        if (in_array("ROLE_ADMIN", $rolesTab)){
+
+            $redirection = new RedirectResponse($this->router->generate('admin'));
+        }
+
+        if ((in_array("ROLE_ETUDIANT",  $rolesTab)) || (in_array("ROLE_PROFESSEUR",  $rolesTab))){
+
+            $redirection = new RedirectResponse($this->router->generate('bibliotheque_profil'));
+        }
+
+        return $redirection;
+    }
+}
