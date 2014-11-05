63b85d5e2ab0041711e0a9770bb821c9427d982a
khalef <khalef.cherif@gmail.com>
2014-11-05 17:08:38 +0100

    formulaire contact

--- src/Bibliotheque/UserBundle/Form/EnquiryType.php
+++ src/Bibliotheque/UserBundle/Form/EnquiryType.php
@@ -11,10 +11,10 @@ class EnquiryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
-        $builder->add('email', 'email');
-        $builder->add('subject');
-        $builder->add('body', 'textarea');
+        $builder->add('name', 'text', array('label' => 'Nom : '));
+        $builder->add('email', 'email', array('label' => 'Email : '));
+        $builder->add('subject','text', array( 'label' => 'Sujet : '));
+        $builder->add('body', 'textarea',array('label' => 'Message : '));
     }
 
     public function getName()
