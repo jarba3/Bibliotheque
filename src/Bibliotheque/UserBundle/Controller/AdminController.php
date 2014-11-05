b5e40eda7ecd163ad664eb617d6045f35e489d99
jarba3 <bjobard@gmail.com>
2014-11-05 12:01:50 +0100

    Modif livre

--- src/Bibliotheque/UserBundle/Controller/AdminController.php
+++ src/Bibliotheque/UserBundle/Controller/AdminController.php
@@ -18,11 +18,11 @@ use Bibliotheque\UserBundle\Entity\Editeur;
 class AdminController extends Controller
 {
 	public function adminAction()
-	{
+	{	
 		return $this->render('UserBundle:Admin:admin.html.twig');
 	}
 
-	public function admin_ajout_livreAction(request $request)
+	public function admin_ajout_livreAction(Request $request)
 	{
 
 		$livre = new Livres();
@@ -118,6 +118,122 @@ class AdminController extends Controller
 		return $this->render('UserBundle:Admin:admin_ajout_livre.html.twig', array('form' => $form->createView()));
 	}
 	
+	public function admin_modif_livreAction(Request $request)
+	{
+		$search = $this->createFormBuilder()
+								->add('recherche', 'search', array('required' => true))
+								->add('save', 'submit', array('label' => 'Rechercher','attr' => array('class' => 'submit spacer')))
+								->getForm();
+
+		$nom = $request->get('form')['recherche'];
+
+		$search->handleRequest($request);	
+		
+		if($search->isValid()){
+
+				return $this->redirect($this->generateUrl('bibliotheque_admin_modif_livre_form', array('nom' => $nom)));
+			
+		}
+
+		return $this->render('UserBundle:Admin:admin_modif_livre.html.twig', array('search' => $search->createView()));
+	}
+
+	public function admin_modif_livre_formAction($nom, Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
+	    $livre = $em->getRepository('UserBundle:Livres')->findByTitre($nom)[0];
+	    
+
+		$form = $this->createFormBuilder($livre)
+					->add('titre', 'text', array('required' => true))
+					->add('isbn', 'text', array('required' => true))
+					->add('description', 'textarea', array('required' => true))
+					->add('dateparution', 'date', array('required' => true))
+					->add('theme', 'choice', array(
+						'choices' => array(
+										'actu_politique_societe' => 'Actu, Politique et Société',
+										'adolescents' => 'Adolescents',
+										'art_musique_cinema' => 'Art, Musique et Cinéma',
+										'bandes_dessinees' => 'Bandes dessinées',
+										'beaux_livres' => 'Beaux livres',
+										'cuisine_vins' => 'Cuisine et Vins',
+										'dictionnaires_langues_encyclopedies' => 'Dictionnaires, langues et encyclopédies',
+										'droit' => 'Droit',
+										'entreprise_bourse' => 'Entreprise et Bourse',
+										'erotisme' => 'Erotisme',
+										'esotérisme_paranormal' => 'Esotérisme et Paranormal',
+										'etudes_superieures' => 'Etudes supérieures',
+										'famille_sante_bien-etre' => 'Famille, Santé et Bien-être',
+										'fantasy_terreur' => 'Fantasy et Terreur',
+										'histoire' => 'Histoire',
+										'humour' => 'Humour',
+										'informatique_internet' => 'Informatique et Internet',
+										'litterature' => 'Littérature',
+										'litterature_sentimentale' => 'Littérature sentimentale',
+										'livres_enfants' => 'Livres pour enfants',
+										'loisirs_creatifs_decoration_bricolage' => 'Loisirs créatifs, décoration et bricolage',
+										'manga' => 'Manga',
+										'nature_animaux' => 'Nature et animaux',
+										'policier_suspense' => 'Policier et Suspense',
+										'religions_spiritualites' => 'Religions et Spiritualités',
+										'science-fiction' => 'Science-Fiction',
+										'sciences_humaines' => 'Sciences humaines',
+										'sciences_techniques_Medecine' => 'Sciences, Techniques et Médecine',
+										'scolaire_parascolaire' => 'Scolaire et Parascolaire',
+										'sports_passions' => 'Sports et passions',
+										'tourisme_voyages' => 'Tourisme et Voyages',
+							)), array('required' => true))
+					->add('auteur', 'entity', array(
+							'class' => 'UserBundle:Auteur',
+							'property' => 'nom',
+							'expanded' => false,
+							'multiple' => false,
+							'data_class' => null
+						))
+					->add('editeur', 'entity', array(
+							'class' => 'UserBundle:Editeur',
+							'property' => 'nom',
+							'expanded' => false,
+							'multiple' => false,
+							'data_class' => null
+						))
+					->add('save', 'submit', array('label' => 'Enregistrer', 'attr' => array('class' => 'submit spacer')))
+					// ->add('image', 'file', array('required' => true))
+					->getForm();
+
+		
+
+		$form->handleRequest($request);
+
+
+
+		if($form->isValid()){
+			
+			$dir = 'bundles/bibliotheque/images';
+			$file = $form['image']->getData();
+
+			$extension = $file->guessExtension();
+				if (!$extension) {
+				    $extension = 'jpeg';
+				}
+			$nomImage = rand(1, 99999).'.'.$extension;
+
+			$file->move($dir, $nomImage);
+
+			$livre->setUrlimage($dir.'/'.$nomImage);
+			$livre->setAltimage($livre->getTitre());
+
+
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($livre);
+			$em->flush();
+
+			return $this->redirect($this->generateUrl('bibliotheque_admin_modif_livre'));
+		}
+
+		return $this->render('UserBundle:Admin:admin_modif_livre_form.html.twig', array('form' => $form->createView()));
+	}
+
 	public function admin_ajout_auteurAction(request $request)
 	{
 		$auteur = new Auteur;
