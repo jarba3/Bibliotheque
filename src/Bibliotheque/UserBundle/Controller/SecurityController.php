597ac2b8ba9a4e52368765d918c2f851c1fc2540
jarba3 <bjobard@gmail.com>
2014-11-11 23:53:31 +0100

    Ajout liens profil admin

--- src/Bibliotheque/UserBundle/Controller/SecurityController.php
+++ src/Bibliotheque/UserBundle/Controller/SecurityController.php
@@ -76,7 +76,16 @@ class SecurityController extends Controller
 									->add('telephone', 'number', array('required' => true))
 									->add('email', 'text', array('required' => true))
 									->add('username', 'text', array('required' => true))
-									->add('password', 'password', array('required' => true))
+									->add('password', 'repeated', array(
+										    'type' => 'password',
+										    'invalid_message' => 'Les mots de passe doivent correspondre',
+										    'options' => array(
+										    	'required' => true,
+										    	'read_only' => true,
+										    ),
+										    'first_options'  => array('label' => 'Mot de passe'),
+										    'second_options' => array('label' => 'Mot de passe (validation)'),
+										))
 									->add('save', 'submit', array(
 														'label' => 'Enregistrer',
 												  'attr' => array(
